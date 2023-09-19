65d0ffe8d4c62e3203ccb659f936f2e54ada4cbe
Antonela10 <antonelamilicevic0@gmail.com>
2023-09-19 14:01:33 +0200

    settings changed

--- movies_platform/settings.py
+++ movies_platform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_**=7qj&vtd8foryv$^@=ap5p7xg+-6w*43#*#tx+eau6(#j*7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
