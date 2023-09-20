b379e0f32dbc4ba37db097ee463b394360f4f254
Antonela10 <antonelamilicevic0@gmail.com>
2023-09-20 11:38:05 +0200

    context_processor changes

--- movies/context_processors.py
+++ movies/context_processors.py
@@ -1,15 +1,7 @@
 from .models import Movies
 
 def genres_list(request):
-    movies = Movies.objects.all()
-    all_genres = []
-    unique_genres = []
-    for movie in movies:
-        movie_genre = movie.genre
-        all_genres.append(movie_genre)
-        split_genres_list = [genres.split(',') for genres in all_genres]
-        flattened_list = [item for sublist in split_genres_list for item in sublist]
-        unique_genres = sorted(list(set(filter(None, flattened_list))))
+    unique_genres = ['action', 'adventure', 'animation', 'biography', 'comedy', 'crime', 'documentary', 'drama', 'family', 'fantasy', 'history', 'horror', 'music', 'musical', 'mystery', 'romance', 'sci-fi', 'short', 'sport', 'thriller', 'war', 'western']
     return {
         "unique_genres": unique_genres
     }
